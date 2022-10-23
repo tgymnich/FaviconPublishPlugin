b9cc59000c594b9c7dbf45927e234018e990557b
Tim Gymnich <tgymnich@icloud.com>
2022-10-24 01:09:24 +0200

    update package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FaviconPublishPlugin",
+    platforms: [.macOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,14 +14,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0"),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.9.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FaviconPublishPlugin",
-            dependencies: ["Publish"]),
+            dependencies: [.product(name: "Publish", package: "publish")]),
         .testTarget(
             name: "FaviconPublishPluginTests",
             dependencies: ["FaviconPublishPlugin"]),
